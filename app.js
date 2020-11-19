7d9abcbb3c420d185946ced2886c45cc28df8987
CryoApe <zulfiqar.aji@gmail.com>
2020-11-19 20:36:31 +0700

    LBE_NCC

---
+++ app.js
@@ -0,0 +1,34 @@
+const toTop = document.querySelector(".logo");
+
+toTop.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+});
+
+const dropDown = document.getElementById("lis");
+
+dropDown.addEventListener("mouseover", isActive);
+dropDown.addEventListener("mouseout", isInactive);
+
+function isActive() {
+    document.getElementById("regions").classList.toggle("active");
+}
+
+function isInactive() {
+    document.getElementById("regions").removeEventListener(dropDown);
+}
+
+// 2 fungsi yang belom kelar
+const firstRegion = document.getElementsById("kanto");
+firstRegion.addEventListener("click", function() {
+    window.open(document.location.href("./list.html")),
+    document.getElementsById("region_1").classList.toggle("active")
+});
+
+const secondRegion = document.getElementsById("johto");
+secondRegion.addEventListener("click", function() {
+    window.open(document.location.href("./list.html")),
+    document.getElementsById("region_2").classList.toggle("active")
+});
